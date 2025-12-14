4625ebdda1883354d4031e9333368c13a5f2a3e5
Bivek Gurung <bivek.tamu@gmail.com>
2025-12-14 23:24:36 +1100

    Removed a UI glitch bug in sign in and dashboard page

--- src/pages/admin/Private.tsx
+++ src/pages/admin/Private.tsx
@@ -9,7 +9,6 @@ import { useToasts } from "../../store/slices/toastSlice"
 import ToastComponent from "../../components/ui/Toast"
 import { Status, Role } from '../../store/types'
 import { useStoreDispatch } from '../../store'
-import ProgressLoader from '../../components/ui/ProgressLoader'
 import Preloader from '../../components/ui/Preloader'
 import AdminLogo from '../../components/ui/AdminLogo'
 
@@ -24,66 +23,53 @@ const PrivateRoute = () => {
     const [isOpen, setIsOpen] = useState(false)
 
     useEffect(() => {
-        if (status === Status.IDLE)
+        if (status === Status.IDLE) {
             dispatch(getAuthStatus())
-    }, [status])
-
-
-    useEffect(() => {
-        if ((status === Status.FULFILLED && !isLoggedIn) || status === Status.REJECTED) {
+        }
+        else if (status === Status.REJECTED) {
             navigate('/admin/login')
         }
-
-    }, [isLoggedIn, status])
-
-    useEffect(() => {
-
-        if (authUser && authUser.role !== Role.ADMIN) {
-            navigate('/admin/login')
+        else if (status === Status.FULFILLED) {
+            if (!isLoggedIn || (isLoggedIn && authUser?.role !== Role.ADMIN)) {
+                navigate('/admin/login')
+            }
         }
-    }, [authUser])
+    }, [status, dispatch, navigate, isLoggedIn, authUser])
+
 
     useEffect(() => {
         setIsOpen(false)
-
         if (pathname === '/admin' || pathname === '/admin/') {
-            return navigate('/admin/dashboard')
+            return navigate('/admin/dashboard', { replace: true })
         }
-    }, [pathname])
+    }, [pathname, navigate])
 
-    if (status === Status.FULFILLED && isLoggedIn && authUser?.role === Role.ADMIN) {
+    if (status !== Status.FULFILLED) return <Preloader />
 
-        return (
-            <div className='admin-wrapper pt-12 md:pt-0'>
+    return (
+        <div className='admin-wrapper pt-12 md:pt-0'>
 
-                {
-                    allToasts?.length > 0 && <ToastComponent toasts={allToasts} />
-                }
-                <header className='px-4 py-2 flex items-center justify-between md:hidden fixed top-0 left-0 w-full bg-white z-10'>
-                    <AdminLogo />
-                    <button type="button" id='burger-menu' className={`lg:hidden ${isOpen ? 'active' : ''}`} onClick={() => setIsOpen(!isOpen)}>
-                        <span></span>
-                    </button>
-                </header>
+            {
+                allToasts?.length > 0 && <ToastComponent toasts={allToasts} />
+            }
+            <header className='px-4 py-2 flex items-center justify-between md:hidden fixed top-0 left-0 w-full bg-white z-10'>
+                <AdminLogo />
+                <button type="button" id='burger-menu' className={`lg:hidden ${isOpen ? 'active' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+                    <span></span>
+                </button>
+            </header>
 
-                <Sidebar isOpen={isOpen} />
+            <Sidebar isOpen={isOpen} />
 
-                <section className="w-full pl-4  md:pl-[200px] lg:pl-[240px] xl:pl-[340px] pb-4 lg:pb-12 xl:pr-12 pr-4 pt-8">
-                    <div className="h-[72px] flex items-center mb-12 justify-between">
-                        <BreadCrumbs />
-                    </div>
+            <section className="w-full pl-4  md:pl-[200px] lg:pl-[240px] xl:pl-[340px] pb-4 lg:pb-12 xl:pr-12 pr-4 pt-8">
+                <div className="h-[72px] flex items-center mb-12 justify-between">
+                    <BreadCrumbs />
+                </div>
+                <Outlet />
+            </section>
 
-                    {
-                        status !== Status.FULFILLED ? <ProgressLoader cssClass='mt-32' /> : <Outlet />
-                    }
-
-
-                </section>
-
-            </div>
-        )
-    }
-    else return <Preloader />
+        </div>
+    )
 }
 
 export default PrivateRoute
